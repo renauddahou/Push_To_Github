2475a8fa94ead5169a984026eecdc2c494618d86
Catherine-Bailey428 <90729618+Catherine-Bailey428@users.noreply.github.com>
2021-09-15 09:36:51 -0400

    Update TweepyStreamTutorial.py
    Had to hide secret keys

--- TweepyStreamTutorial.py
+++ TweepyStreamTutorial.py
@@ -1,8 +1,8 @@
 import tweepy 
 consumer_key = 'tx75v4aD7qk81A5rfiITGWhRm'
-secret_key = 'mJ3OJ7YG36XmX7Twk06yMv8XcoG6PkAf2DNW2pusIofbJdrm8a'
+secret_key = '#SECRET KEY'
 access_token = "343351837-RDEgyiJZnW1RmkzrJB7TAhUs2iI6eBd7APG18COc"
-token_secret = "cXc2RCv7pzpvhOjrTOZyjDG3kCeLbgaK62iwuPpSsInhv"
+token_secret = "SECRET TOKEN"
 auth = tweepy.OAuthHandler(consumer_key, secret_key)
 auth.set_access_token(access_token, token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
