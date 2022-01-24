16ddf94a7118e48a2c581496ad86a5f8c6c5bb43
Catherine-Bailey428 <90729618+Catherine-Bailey428@users.noreply.github.com>
2022-01-24 17:45:56 -0500

    Update TwitterSentiment.py

--- TwitterSentiment.py
+++ TwitterSentiment.py
@@ -1,9 +1,9 @@
 import tweepy
 from textblob import TextBlob
 api_key = 'tx75v4aD7qk81A5rfiITGWhRm'
-secret_key = 'mJ3OJ7YG36XmX7Twk06yMv8XcoG6PkAf2DNW2pusIofbJdrm8a'
-access_token = '343351837-RDEgyiJZnW1RmkzrJB7TAhUs2iI6eBd7APG18COc'
-token_secret = 'cXc2RCv7pzpvhOjrTOZyjDG3kCeLbgaK62iwuPpSsInhv'
+secret_key = '#secret_key'
+access_token = '#access_token'
+token_secret = '#token_secret'
 auth = tweepy.OAuthHandler(api_key, secret_key)
 auth.set_access_token(access_token, token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
